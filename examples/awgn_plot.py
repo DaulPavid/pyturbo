1a09dfd796e5bbd80e07f87743627b1357a9ae8d
Paul David <puddavid@gmail.com>
2018-10-10 22:26:51 -0400

    Implement argument parsing in the AWGN example

--- examples/awgn_plot.py
+++ examples/awgn_plot.py
@@ -4,14 +4,14 @@
 
 #!/usr/bin/env python3
 
+import argparse
 import matplotlib.pyplot as plot
 
 from turbo import AWGN
 
 
-if __name__ == "__main__":
-    channel = AWGN(2)
-    input_vector = 10 * [1, 1, 0, 0, 1, 0, 1, 0, 1, 1]
+def create_awgn_plot(input_vector, SNR_dB):
+    channel = AWGN(SNR_dB)
     input_vector = list(map(float, input_vector))
     input_vector = channel.convert_to_symbols(input_vector)
 
@@ -25,3 +25,23 @@ if __name__ == "__main__":
     plot.plot(output_vector, 'r.')
     plot.title("output_vector")
     plot.show()
+
+
+def options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--snr", type=float,
+        default=5.0,
+        help="SNR [dB] in an AWGN channel"
+    )
+    parser.add_argument(
+        "-i", "--input", nargs="+", type=int,
+        default=10 * [1, 1, 0, 0, 1, 0, 1, 0, 1, 1],
+        help="Input vector to the AWGN channel"
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = options()
+    create_awgn_plot(args.input, args.snr)
