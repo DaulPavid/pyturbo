Fix AWGN scaling

examples/ber_plot.py
@@ -25,7 +25,8 @@ def create_ber_plot(plot_params):
     encoder = TurboEncoder(interleaver)
     decoder = TurboDecoder(interleaver)
 
-    num_errors = np.zeros(len(snr_range))
+    coded_errors = np.zeros(len(snr_range))
+    uncoded_errors = np.zeros(len(snr_range))
 
     for n in range(len(snr_range)):
         for _ in range(num_trials):
@@ -37,27 +38,38 @@ def create_ber_plot(plot_params):
             channel_vector = list(map(float, encoded_vector))
             channel_vector = channel.convert_to_symbols(channel_vector)
 
+            uncoded_vector = list(map(float, input_vector))
+            uncoded_vector = channel.convert_to_symbols(uncoded_vector)
+            uncoded_vector = channel.execute(uncoded_vector)
+
             channel_vector = channel.execute(channel_vector)
 
             decoded_vector = decoder.execute(channel_vector)
             decoded_vector = [int(b > 0.0) for b in decoded_vector]
+            uncoded_vector = [int(b > 0.0) for b in uncoded_vector]
 
             decoder.reset()
 
-            error_count = sum([x ^ y for x, y in zip(input_vector, decoded_vector)])
-            num_errors[n] = num_errors[n] + error_count
+            coded_error_count = sum([x ^ y for x, y in zip(input_vector, decoded_vector)])
+            uncoded_error_count = sum([x ^ y for x, y in zip(input_vector, uncoded_vector)])
+
+            coded_errors[n] = coded_errors[n] + coded_error_count
+            uncoded_errors[n] = uncoded_errors[n] + uncoded_error_count
 
         print("Finished {} trials for SNR = {:8.2f} dB ...".format(num_trials, snr_range[n]))
 
-    ber_values = num_errors / (num_trials * block_size)
+    coded_ber_values = coded_errors / (num_trials * block_size)
+    uncoded_ber_values = uncoded_errors / (num_trials * block_size)
 
-    plot.plot(snr_range, ber_values, "r.-")
+    plot.plot(snr_range, coded_ber_values, "r.-", label="Coded BPSK")
+    plot.plot(snr_range, uncoded_ber_values, "bo-", label="Uncoded BPSK")
     plot.yscale("log")
-    plot.title("Turbo Codes Performance for R=1/3, Trials={}".format(num_trials))
+    plot.title("Turbo Codes Performance for R=1/3, Block={}, Trials={}".format(block_size, num_trials))
     plot.xlabel("SNR [dB]")
     plot.ylabel("Bit Error Rate (BER)")
     plot.grid(b=True, which="major", linestyle="-")
     plot.grid(b=True, which="minor", linestyle="--")
+    plot.legend()
     plot.show()
 
 
@@ -65,12 +77,12 @@ def options():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-s", "--snr", nargs=3, type=float,
-        default=[-10.0, 10.0, 20],
+        default=[-10.0, 20.0, 20],
         help="SNR range [dB] in an AWGN channel"
     )
     parser.add_argument(
         "--block-size", type=int,
-        default=40,
+        default=100,
         help="Block size (size of interleaver)"
     )
     parser.add_argument(

turbo/awgn.py
@@ -13,7 +13,7 @@ class AWGN:
         return np.add(np.multiply(vector, 2), -1)
 
     def __init__(self, noise_dB):
-        self.scale = 1.0 / (10.0**(noise_dB / 10.0))
+        self.scale = 1.0 / (10.0**(noise_dB / 20.0))
 
     def execute(self, vector):
         noise = np.random.normal(0, 1, len(vector))